e28450237ebc653bcd000b8ab336cc406093f69d
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2024-11-10 00:07:35 +0100

    Top Bar scrolling functionality

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const topBarElem = document.querySelector('[data-topBar]');
 
 // top bar functionality
 const topBarScrolling = () => {
-    const atTop = window.screenY === 0;
-    topBarElem.classList.toggle('bar-visible', atTop);
-    topBarElem.classList.toggle('bar-hidden', !atTop);
+    const atTop = window.scrollY === 0;
+    topBarElem.classList.toggle('js-bar-visible', atTop);
+    topBarElem.classList.toggle('js-bar-hidden', !atTop);
 }
 // using requestAnimationFrame for performace issues in this context
 window.addEventListener('scroll', () => requestAnimationFrame(topBarScrolling));
