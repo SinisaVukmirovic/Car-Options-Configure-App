25cd46892e473021fd9dddf77a4ee8ebde3b6f96
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2024-11-10 18:18:32 +0100

    Color selection and image change functionality

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const topBarElem = document.querySelector('[data-topBar]');
+const exteriorOptions = document.querySelector('[data-exterior-options]');
+const interiorOptions = document.querySelector('[data-interior-options]');
+const exteriorImgElem = document.querySelector('[data-exterior]');
+const interiorImgElem = document.querySelector('[data-interior]');
 
 // top bar functionality
 const topBarScrolling = () => {
@@ -8,3 +12,50 @@ const topBarScrolling = () => {
 }
 // using requestAnimationFrame for performace issues in this context
 window.addEventListener('scroll', () => requestAnimationFrame(topBarScrolling));
+
+// obj of the images for the exterior
+const exteriorImgs = {
+    'Stealth Grey': './assets/images/model-y-stealth-grey.jpg',
+    'Pearl White': './assets/images/model-y-pearl-white.jpg',
+    'Deep Blue Metalic': './assets/images/model-y-deep-blue-metallic.jpg',
+    'Solid Black': './assets/images/model-y-solid-black.jpg',
+    'Ultra Red': './assets/images/model-y-ultra-red.jpg',
+    'Quicksilver': './assets/images/model-y-quicksilver.jpg', 
+}
+// obj of thw images for the interior
+const interiorImgs = {
+    Dark: './assets/images/model-y-interior-dark.jpg',
+    Light: './assets/images/model-y-interior-light.jpg'
+}
+
+// handle color selection functionality (for both, exterior and interior)
+const handleOptionSelection = (e) => {
+    let button;
+
+    if (e.target.tagName === 'IMG') {
+        button = e.target.closest('button');
+    } else if (e.target.tagName === 'BUTTON') {
+        button = e.target;
+    }
+
+    if (button) {
+        const allButtons = e.currentTarget.querySelectorAll('button');
+        allButtons.forEach(btn => btn.classList.remove('selected'));
+        button.classList.add('selected');
+
+        // exterior image change
+        if (e.currentTarget === exteriorOptions) {
+            const selectedColor = button.querySelector('img').alt;
+            exteriorImgElem.src = exteriorImgs[selectedColor];
+        }
+        // interior image change
+        if (e.currentTarget === interiorOptions) {
+            const selectedColor = button.querySelector('img').alt;
+            interiorImgElem.src = interiorImgs[selectedColor];
+        }
+    }
+}
+
+exteriorOptions.addEventListener('click', handleOptionSelection);
+interiorOptions.addEventListener('click', handleOptionSelection);
+
